444d213852e34d0b664823d9ac7f45ab1e42ca67
Gerardo Maranan, Jr <gerardojr.maranan@gmail.com>
2023-02-18 03:13:00 +0800

    Added more

--- tests/InputTests.cs
+++ tests/InputTests.cs
@@ -8,6 +8,9 @@ public class InputTests
     [InlineData("{{}", "one bracket pair was not closed", false)]
     [InlineData("", "no brackets in the string will return True", true)]
     [InlineData("{abc...xyz}", "non-bracket characters are ignored appropriately", true)]
+    [InlineData("{{}}}", "closed too much", false)]
+    [InlineData("{{{{{{{{{{{}}}}}}}}}}}", "yeet", true)]
+    [InlineData("{a{b{c{d{e{f{g{h{j{k{l}m}n}o}p}q}r}s}t}u}v}", "this wont break it", true)]
     public void Can_Validate_Input_Content(string content, string reason, bool expected)
     {
         var input = new Input(content);
